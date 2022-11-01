1ed07f9dee2d44e243d6728236c6f62c684ff482
SeaCoffee <sapienti8sat@gmail.com>
2022-11-01 13:16:56 +0200

    homework js 1

---
+++ JS/lesson1.js
@@ -0,0 +1,63 @@
+let greetings = 'hello';
+console.log(greetings);
+
+let okten = 'owu';
+console.log(okten);
+
+let ukraine = 'ua';
+console.log(ukraine);
+
+let number_1 = 1;
+console.log(number_1);
+
+let number_2 = 10;
+console.log(number_2);
+
+let number_3 = -999;
+console.log(number_3);
+
+let number_4 = 123;
+console.log(number_4);
+
+let pi = 3.14;
+console.log(pi);
+
+let number_5 = 2.7;
+console.log(number_5);
+
+let number_6 = 16
+console.log(number_6);
+
+let fact = "true"
+console.log(fact);
+
+let not_a_fact = "false"
+console.log(not_a_fact);
+
+let firstName = 'Lesia'
+let middleName = 'Uriivna'
+let lastName = 'Khorishchenko'
+
+const PERSON = lastName + " " + firstName + " " + middleName
+console.log(PERSON)
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let introduce1;
+introduce1 = promt("What is your first_name?");
+console.log("Your name is " + introduce1)
+
+let introduce2;
+introduce2 = promt("What is your middle_name?");
+console.log("Your middle name is " + introduce2)
+
+let introduce3;
+introduce3 = promt("What is your last_name?");
+console.log("Your last name is " + introduce3)
+
